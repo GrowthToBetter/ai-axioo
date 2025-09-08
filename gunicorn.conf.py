cabad2d0edd39f2bb103edc37e64e33d7336b616
GrowthToBetter <baratrahjaga@gmail.com>
2025-09-08 18:51:49 +0700

    fix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -25,7 +25,7 @@ loglevel = "info"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
 
 # Process naming
-proc_name = "emergency-nlp-system"
+proc_name = "Lomba"
 
 # Server mechanics
 daemon = False
--- start_render.sh
+++ start_render.sh
@@ -8,4 +8,7 @@ fi
 
 # Start the application with Gunicorn
 echo "Starting Gunicorn server..."
-gunicorn --config gunicorn.conf.py emergency-nlp-system:flask_app
+exec gunicorn --config gunicorn.conf.py "Lomba:flask_app" \
+    --bind "0.0.0.0:${PORT:-5000}" \
+    --workers "${WEB_CONCURRENCY:-2}" \
+    --timeout 120
